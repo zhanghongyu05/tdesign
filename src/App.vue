40d99be34669deb5a88a56cb03ddd3f96e782b1e
v_zhanghongyu05 <v_zhanghongyu05@baidu.com>
2022-12-01 11:24:55 +0800

    基础搭建

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <t-config-provider :global-config="{ classPrefix: 't' }">
+    111
+  </t-config-provider>
+</template>
+
+<script setup lang="ts">
+import {
+  AddIcon,
+  CloudUploadIcon,
+  DiscountIcon,
+  CloudDownloadIcon,
+} from "tdesign-icons-vue-next";
+ 
+</script>
+
